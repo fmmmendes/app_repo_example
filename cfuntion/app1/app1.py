29079c849bfd7888394730d53e92c88bf79a2554
Filipe Mendes <fmppa.mendes@gmail.com>
2022-04-09 03:03:55 +0100

    update app1 new release

---
+++ cfuntion/app1/app1.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def square(x):
+    """_summary_
+
+    Args:
+        x (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return x*x
+
+
+def read_txt_file(pathfile='readme.txt'):
+    """_summary_
+
+    Args:
+        pathfile (str, optional): _description_. Defaults to 'readme.txt'.
+    """
+    f = open(pathfile,"r")
+    lines = f.readlines()
+    print(lines)
+    
+    return lines
+
+
+if __name__ == "__main__":
+    
+    read_txt_file(pathfile = sys.argv[1])
