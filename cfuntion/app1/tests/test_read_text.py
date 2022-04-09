296ae88185d92c389322bcd232552d5cd3807a9c
Filipe Mendes <fmppa.mendes@gmail.com>
2022-04-09 04:38:32 +0100

    app1 test fix

--- cfuntion/app1/tests/test_read_text.py
+++ cfuntion/app1/tests/test_read_text.py
@@ -2,5 +2,5 @@ from my_funtions import read_txt_file
 
 
 def test_read_txt_file():
-    assert read_txt_file(pathfile = 'readme.txt') == ['This is app 1, relase 2']
+    assert read_txt_file(pathfile = 'readme.txt') == ['This is app 1, relase 3']
     
